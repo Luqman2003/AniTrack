9bc159ea8f80f3cbec3e3aa6f9702e6fdfd8a257
Luqman2003 <luqmanshibly05@gmail.com>
2024-09-04 14:56:22 -0700

    feat: first commit

---
+++ src/main/java/com/example/anitrack/AniTrackApplication.java
@@ -0,0 +1,13 @@
+package com.example.anitrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AniTrackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AniTrackApplication.class, args);
+    }
+
+}
