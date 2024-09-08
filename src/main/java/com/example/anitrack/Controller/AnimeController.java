1360f42f71d5df77a3fe089e033f4643780f061d
Luqman2003 <luqmanshibly05@gmail.com>
2024-09-07 21:26:24 -0700

    fix: removed wildcard import

--- src/main/java/com/example/anitrack/Controller/AnimeController.java
+++ src/main/java/com/example/anitrack/Controller/AnimeController.java
@@ -1,7 +1,12 @@
 package com.example.anitrack.Controller;
 
 import com.example.anitrack.Service.JikanService;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
